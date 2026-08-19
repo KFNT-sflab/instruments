8784c2bba6aabbb021329eab69b27423fb18e123
Emil Varga <varga.emil@gmail.com>
2026-08-19 16:51:49 +0200

    Formatting, and project details.
    Authors and project script for instrument server

--- src/sflab_instruments/InstrumentServer.py
+++ src/sflab_instruments/InstrumentServer.py
@@ -1,38 +1,40 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 Created on Sat Dec 16 20:11:24 2023
 
 @author: emil
 """
 
-from multiprocessing.connection import Listener
-
-import os
-import threading as thr
-from queue import Queue
-import tempfile
 import logging as log
-import pyvisa as visa
+import os
 import pickle
+import tempfile
+import threading as thr
+from multiprocessing.connection import Listener
+from queue import Queue
+
+import pyvisa as visa
+
 
 class InstrumentClientListener:
     """
     Class for establishing connections to clients and starting client handler threads.
     """
-    def __init__(self, instruments, port=0, address=None, port_filename='instrument_server_port.txt'):
+
+    def __init__(self, instruments, port=0, address=None, port_filename="instrument_server_port.txt"):
         if address is None:
-            address = 'localhost'
-        
+            address = "localhost"
+
         self.instruments = instruments
         self.port = port
         self.port_filename = os.path.join(tempfile.gettempdir(), port_filename)
-        
+
         self.address = (address, self.port)
-        self.handlers = {} # running handlers
-        self.end_event = thr.Event() # for signaling running handlers
-        self.handler_id = 0 # id of the next handler
-        self.finished_handlers = Queue() # handler threads which should be joined            
+        self.handlers = {}  # running handlers
+        self.end_event = thr.Event()  # for signaling running handlers
+        self.handler_id = 0  # id of the next handler
+        self.finished_handlers = Queue()  # handler threads which should be joined
+
     def start(self):
         """
         Opens the listener and saves the port number to a temporary file. Connections
@@ -41,12 +43,13 @@ class InstrumentClientListener:
         """
         self.listener = Listener(self.address)
         self.address = self.listener.address
-        if self.port == 0: #port 0 means that the port was assigned automatically by the OS
-            self.port = self.listener.address[1] #save the actual port
-        with open(self.port_filename, 'w') as port_file:
+        if self.port == 0:  # port 0 means that the port was assigned automatically by the OS
+            self.port = self.listener.address[1]  # save the actual port
+        with open(self.port_filename, "w") as port_file:
             port_file.write(f"{self.port}\n")
             port_file.write(f"{self.address[0]}\n")
-        
+        print(f"Strting server on port {self.port}.")
+
     def accept(self):
         """
         Accepts a connection and starts its handler in a separate thread.
@@ -55,19 +58,19 @@ class InstrumentClientListener:
         conn = self.listener.accept()
         log.info(f"Accepted {self.listener.last_accepted}")
         handler_name = f"handler {self.handler_id}"
-        c = InstrumentClientHandler(conn, self.end_event, name=handler_name,
-                                    finished=self.finished_handlers, 
-                                    visa_instruments=self.instruments)
+        c = InstrumentClientHandler(
+            conn, self.end_event, name=handler_name, finished=self.finished_handlers, visa_instruments=self.instruments
+        )
         self.handlers[handler_name] = c
         self.handlers[handler_name].start()
         self.handler_id += 1
-    
+
     def join_finished_handlers(self):
         while not self.finished_handlers.empty():
             name = self.finished_handlers.get()
             log.info(f"Joining {name}")
             self.handlers.pop(name).join()
-    
+
     def close_server(self):
         log.info("Joining remaining handlers.")
         self.end_event.set()
@@ -76,57 +79,64 @@ class InstrumentClientListener:
         log.info(f"Removing {self.port_filename}")
         os.remove(self.port_filename)
         log.info("Instrument server shutting down.")
-    
+
     def loop(self):
         while True:
-            self.accept() #blocking
+            self.accept()  # blocking
             self.join_finished_handlers()
-    
+
     def __enter__(self):
         return self
+
     def __exit__(self, exc_type, exc_value, traceback):
         self.close_server()
         if exc_type is None:
             return True
-        else: #propagate whatever exception happened
+        else:  # propagate whatever exception happened
             return False
+
 
 class RefCountedInstrument:
     "Simple wrapper for VISA resources that counts how many clients are open to it."
+
     def __init__(self, dev):
         self.dev = dev
         self.ref_counter = 1
+
     def inc(self):
         self.ref_counter += 1
+
     def refclose(self):
         self.ref_counter -= 1
         if self.ref_counter == 0:
             self.dev.close()
         return self.ref_counter
 
+
 class VISAInstruments:
     "Manager for all VISA communications, to be shared by all client handlers"
+
     def __init__(self):
         self.global_visa_lock = thr.Lock()
         self.instruments = {}
         self.rm = visa.ResourceManager()
-    
-    def open_instrument(self, addr: str, conf: dict|None = None) -> None:
+
+    def open_instrument(self, addr: str, conf: dict | None = None) -> None:
         if addr not in self.instruments:
             log.info(f"Opening new instrument at {addr}")
             self.instruments[addr] = RefCountedInstrument(self.rm.open_resource(addr))
         else:
             self.instruments[addr].inc()
             log.info(f"Using already opened instrument at {addr}")
-        
+
         if conf is not None:
             self.configure_instrument(addr, conf)
-    
+
     def close_instrument(self, addr: str) -> None:
         count = self.instruments[addr].refclose()
         if count == 0:
             self.instruments.pop(addr)
-        
+
     def configure_instrument(self, addr: str, conf: dict) -> None:
         """
         Configure an already opened instruments
@@ -146,34 +156,36 @@ class VISAInstruments:
         """
         for attr in conf:
             setattr(self.instruments[addr].dev, attr, conf[attr])
-        
+
     def write(self, addr: str, msg: str) -> None:
         with self.global_visa_lock:
             self.instruments[addr].dev.write(msg)
-    
+
     def read(self, addr: str) -> str:
         with self.global_visa_lock:
             resp = self.instruments[addr].dev.read()
         return resp
-    
-    def query(self, addr:str, msg: str) -> str:
+
+    def query(self, addr: str, msg: str) -> str:
         with self.global_visa_lock:
-            resp =  self.instruments[addr].dev.query(msg)
+            resp = self.instruments[addr].dev.query(msg)
         return resp
-    
+
     def close(self):
         for inst in self.instruments.values():
             inst.dev.close()
         self.rm.close()
-    
+
     def __enter__(self):
         return self
+
     def __exit__(self, exc_type, exc_value, exc_bt):
         self.close()
         if exc_type is None:
             return True
         else:
             return False
+
 
 class InstrumentClientHandler(thr.Thread):
     def __init__(self, conn, end_event, name, finished, visa_instruments):
@@ -183,18 +195,18 @@ class InstrumentClientHandler(thr.Thread):
         self.name = name
         self.finished = finished
         self.visa_instruments = visa_instruments
-    
+
     @staticmethod
     def parse_msg(msg):
-        toks = msg.split(' ')
+        toks = msg.split(" ")
         task = toks[0].upper()
         addr = toks[1]
         if len(toks) > 2:
-            cmd = ' '.join(toks[2:])
+            cmd = " ".join(toks[2:])
         else:
-            cmd = ''
+            cmd = ""
         return task, addr, cmd
-    
+
     def run(self):
         try:
             while True:
@@ -239,13 +251,18 @@ class InstrumentClientHandler(thr.Thread):
             self.conn.close()
             self.finished.put(self.name)
 
-if __name__ == '__main__':
-    log.basicConfig(filename="instrument_server_log.txt",
-                    format="%(asctime)s %(levelname)s:%(message)s",
-                    level=log.INFO)
-    with (VISAInstruments() as instruments, 
-          InstrumentClientListener(instruments, address='') as server
-          ):
-        server.start()
-        server.loop()
-    
+
+def main():
+    log.basicConfig(
+        filename="instrument_server_log.txt", format="%(asctime)s %(levelname)s:%(message)s", level=log.INFO
+    )
+    try:
+        with VISAInstruments() as instruments, InstrumentClientListener(instruments, address="") as server:
+            server.start()
+            server.loop()
+    except KeyboardInterrupt:
+        print("Shutting down...")
+
+
+if __name__ == "__main__":
+    main()
